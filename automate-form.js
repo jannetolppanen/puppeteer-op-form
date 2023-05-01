474cf13505e7b25bc0840edb4d38cd59aa15864d
Janne Tolppanen <janne.ks@gmail.com>
2023-05-01 23:51:49 +0300

    Submitting data works. Need to fix times when its not an even hour

--- automate-form.js
+++ automate-form.js
@@ -1,13 +1,18 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 
-
-
+const form_description = "test description"
+const form_date = '01.06.2023'
+const form_startTime = '08'
+const form_endTime = '16';
 
 (async () => {
     // Launch a new browser instance
     console.log('Launching browser...')
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 35
+    });
 
     // Create a new page
     const page = await browser.newPage();
@@ -18,7 +23,7 @@ require('dotenv').config();
     // Navigate to the login page
     console.log('Navigating to site')
     await page.goto('https://app.op-kevytyrittaja.fi/vahennykset/paivarahat/uusi');
-    
+
 
     // Check if we are in the login page
     console.log('Checking if we got into the login page...')
@@ -35,25 +40,34 @@ require('dotenv').config();
     await Promise.all([
         page.click('#sign_in_form > button'),
         page.waitForNavigation()
-        
+
     ]);
     console.log('Signed in')
-    await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
-    // Navigate to the form page
-    await page.goto('https://example.com/form');
+
+    console.log('Inputting data...')
+
+    // Clear the form fields before typing into them
+    await page.$eval('#daily_allowance_row_description', field => field.value = '');
+    await page.$eval('#daily_allowance_row_trip_start_date', field => field.value = '');
+    await page.$eval('#daily_allowance_row_trip_end_date', field => field.value = '');
+    await page.$eval('#daily_allowance_row_trip_start_time', field => field.value = '');
+    await page.$eval('#daily_allowance_row_trip_end_time', field => field.value = '');
 
     // Fill out the form
-    await page.type('#name', 'Your Name');
-    await page.type('#email', 'youremail@example.com');
-    await page.type('#message', 'Hello, world!');
+    await page.type('#daily_allowance_row_description', form_description);
+    await page.type('#daily_allowance_row_trip_start_date', form_date);
+    await page.type('#daily_allowance_row_trip_end_date', form_date);
+    await page.type('#daily_allowance_row_trip_start_time', form_startTime);
+    await page.type('#daily_allowance_row_trip_end_time', form_endTime);
 
     // Submit the form
     await Promise.all([
-        page.click('#submit-button'),
+        page.click('#daily_allowance_form > div.flexrow.justify--end > button'),
         page.waitForNavigation()
     ]);
+    console.log('Submitted data')
 
     // Close the browser
-    await browser.close();
+    // await browser.close();
 })();
